3b765eaa140e67dfb8c49df5c8a69698ed9be3f1
Ryan Sullivan <ryan@bossalien.com>
2014-10-13 13:46:40 +0100

    got achnor scroll working

--- app/portfolio/portfolio.js
+++ app/portfolio/portfolio.js
@@ -10,4 +10,13 @@ angular.module('myApp.portfolio', ['ngRoute'])
    });
 }])
 
-.controller('MarkdownController',[function() {}]);
+.controller('MarkdownController',['$scope', '$location', '$anchorScroll', function($scope, $location, $anchorScroll) 
+{
+    $scope.scrollTo = function(id) {
+	var old = $location.hash();
+	$location.hash(id);
+	$anchorScroll();
+	//reset to old to keep any additional routing logic from kicking in
+	$location.hash(old);
+    };
+}]);
